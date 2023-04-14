917aac2a91b596c3a9976a93bbbf424a0b7f7b35
KatsuroAlex <katsuroay@mail.ru>
2023-04-14 19:14:30 +0400

    localhost changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const handleErrors = require('./middlewares/error-handler');
 const corsOption = require('./middlewares/cors');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { JWT_SECRET = 'dev-secret', MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { JWT_SECRET = 'dev-secret', MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   JWT_SECRET,
